ce1eb60881b0174b8d493ee8445482585dfc1397
Vaishnavi Kalhapure <vaishnavi02kalhapure@gmail.com>
2025-09-02 15:35:09 +0530

    menu imp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, res, choice;
+    cout << "Calculator APP" << endl;
+    cout << "Enter two numbers:";
+    cout << "\n Enter choice: ";
+    cin >> num1 >> num2;
+    cout<<"Enter Choice";
+    cin>>choice;
+
+    switch (choice)
+    {
+    
+    default:
+        cout<<"Invalid Choice"<<endl;
+        break;
+    }
+    return 0;
+}
